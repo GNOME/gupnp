d93ddc27c399ee1ec47b83ce5c38961387732f82
Jorn Baayen <jorn@openedhand.com>
2006-06-20 08:13:16 +0000

    add service proxy skels
    git-svn-id: https://svn.o-hand.com/repos/gupnp/gupnp@155 d8cb91d7-bff9-0310-92b9-80b65e4482b2

--- libgupnp/gupnp-service-proxy.c
+++ libgupnp/gupnp-service-proxy.c
@@ -41,7 +41,21 @@ struct _GUPnPServiceProxyPrivate {
         xmlDoc *doc;
 
         xmlNode *element;
+
+        gboolean subscribed;
 };
+
+enum {
+        PROP_0,
+        PROP_SUBSCRIBED
+};
+
+enum {
+        SUBSCRIPTION_LOST,
+        LAST_SIGNAL
+};
+
+static guint signals[LAST_SIGNAL];
 
 static const char *
 gupnp_service_proxy_get_location (GUPnPServiceInfo *info)
@@ -94,6 +108,29 @@ gupnp_service_proxy_class_init (GUPnPServiceProxyClass *klass)
         object_class->finalize = gupnp_service_proxy_finalize;
         
         g_type_class_add_private (klass, sizeof (GUPnPServiceProxyPrivate));
+
+        g_object_class_install_property
+                (object_class,
+                 PROP_SUBSCRIBED,
+                 g_param_spec_boolean ("subscribed",
+                                       "Subscribed",
+                                       "TRUE if we are subscribed to the "
+                                       "service",
+                                       FALSE,
+                                       G_PARAM_READWRITE));
+                                       
+        signals[SUBSCRIPTION_LOST] =
+                g_signal_new ("subscription-lost",
+                              GUPNP_TYPE_SERVICE_PROXY,
+                              G_SIGNAL_RUN_LAST,
+                              G_STRUCT_OFFSET (GUPnPServiceProxyClass,
+                                               subscription_lost),
+                              NULL,
+                              NULL,
+                              g_cclosure_marshal_VOID__POINTER,
+                              G_TYPE_NONE,
+                              1,
+                              G_TYPE_POINTER);
 }
 
 static void
@@ -101,6 +138,256 @@ gupnp_service_proxy_info_init (GUPnPServiceInfoIface *iface)
 {
         iface->get_location = gupnp_service_proxy_get_location;
         iface->get_element  = gupnp_service_proxy_get_element;
+}
+
+/**
+ * gupnp_service_proxy_send_action
+ * @proxy: A #GUPnPServiceProxy
+ * @action: An action
+ * @error: The location where to store any error, or NULL
+ * @Varargs: tuples of in parameter name, in paramater type, and in parameter
+ * value, followed by NULL, and then tuples of out paramater name,
+ * out parameter type, and out parameter value location, terminated with NULL
+ *
+ * Sends action @action with parameters @Varargs to the service exposed by
+ * @proxy synchronously.
+ *
+ * Return value: TRUE if sending the action was succesfull.
+ **/
+gboolean
+gupnp_service_proxy_send_action (GUPnPServiceProxy *proxy,
+                                 const char        *action,
+                                 GError           **error,
+                                 ...)
+{
+        g_return_val_if_fail (GUPNP_IS_SERVICE_PROXY (proxy), FALSE);
+        g_return_val_if_fail (action, FALSE);
+
+        return FALSE;
+}
+
+/**
+ * gupnp_service_proxy_send_action_valist
+ * @proxy: A #GUPnPServiceProxy
+ * @action: An action
+ * @error: The location where to store any error, or NULL
+ * @var_args: va_list of tuples of in parameter name, in paramater type, and in
+ * parameter value, followed by NULL, and then tuples of out paramater name,
+ * out parameter type, and out parameter value location
+ *
+ * See gupnp_service_proxy_send_action(); this version takes a va_list for
+ * use by language bindings.
+ *
+ * Return value: TRUE if sending the action was succesfull.
+ **/
+gboolean
+gupnp_service_proxy_send_action_valist (GUPnPServiceProxy *proxy,
+                                        const char        *action,
+                                        GError           **error,
+                                        va_list            var_args)
+{
+        g_return_val_if_fail (GUPNP_IS_SERVICE_PROXY (proxy), FALSE);
+        g_return_val_if_fail (action, FALSE);
+
+        return FALSE;
+}
+
+/**
+ * gupnp_service_proxy_begin_action
+ * @proxy: A #GUPnPServiceProxy
+ * @action: An action
+ * @callback: The callback to call when sending the action has succeeded
+ * or failed
+ * @user_data: User data for @callback
+ * @Varargs: tuples of in parameter name, in paramater type, and in parameter
+ * value, terminated with NULL
+ *
+ * Sends action @action with parameters @Varargs to the service exposed by
+ * @proxy asynchronously, reporting the result by calling @callback.
+ *
+ * Return value: A #GUPnPServiceProxyAction handle. This will be freed
+ * automatically on @callback calling.
+ **/
+GUPnPServiceProxyAction *
+gupnp_service_proxy_begin_action (GUPnPServiceProxy              *proxy,
+                                  const char                     *action,
+                                  GUPnPServiceProxyActionCallback callback,
+                                  gpointer                        user_data,
+                                  ...)
+{
+        g_return_val_if_fail (GUPNP_IS_SERVICE_PROXY (proxy), NULL);
+        g_return_val_if_fail (action, NULL);
+        g_return_val_if_fail (callback, NULL);
+
+        return NULL;
+}
+
+/**
+ * gupnp_service_proxy_begin_action_valist
+ * @proxy: A #GUPnPServiceProxy
+ * @action: An action
+ * @callback: The callback to call when sending the action has succeeded
+ * or failed
+ * @user_data: User data for @callback
+ * @var_args: A va_list of tuples of in parameter name, in paramater type, and 
+ * in parameter value
+ *
+ * See gupnp_service_proxy_begin_action(); this version takes a va_list for
+ * use by language bindings.
+ *
+ * Return value: A #GUPnPServiceProxyAction handle. This will be freed
+ * automatically on @callback calling.
+ **/
+GUPnPServiceProxyAction *
+gupnp_service_proxy_begin_action_valist
+                                   (GUPnPServiceProxy              *proxy,
+                                    const char                     *action,
+                                    GUPnPServiceProxyActionCallback callback,
+                                    gpointer                        user_data,
+                                    va_list                         var_args)
+{
+        g_return_val_if_fail (GUPNP_IS_SERVICE_PROXY (proxy), NULL);
+        g_return_val_if_fail (action, NULL);
+        g_return_val_if_fail (callback, NULL);
+
+        return NULL;
+}
+
+/**
+ * gupnp_service_proxy_end_action
+ * @proxy: A #GUPnPServiceProxy
+ * @action: A #GUPnPServiceProxyAction handle
+ * @error: The location where to store any error, or NULL
+ * @Varargs: tuples of out parameter name, out paramater type, and out parameter
+ * value location, terminated with NULL
+ *
+ * Retrieves the result of @action. The out parameters in @Varargs will be 
+ * filled in, and ff an error occurred, @error will be set.
+ *
+ * Return value: TRUE on success.
+ **/
+gboolean
+gupnp_service_proxy_end_action (GUPnPServiceProxy       *proxy,
+                                GUPnPServiceProxyAction *action,
+                                GError                 **error,
+                                ...)
+{
+        g_return_val_if_fail (GUPNP_IS_SERVICE_PROXY (proxy), FALSE);
+        g_return_val_if_fail (action, FALSE);
+
+        return FALSE;
+}
+
+/**
+ * gupnp_service_proxy_end_action_valist
+ * @proxy: A #GUPnPServiceProxy
+ * @action: A #GUPnPServiceProxyAction handle
+ * @error: The location where to store any error, or NULL
+ * @var_args: A va_list of tuples of out parameter name, out paramater type, 
+ * and out parameter value location
+ *
+ * See gupnp_service_proxy_end_action(); this version takes a va_list for
+ * use by language bindings.
+ *
+ * Return value: TRUE on success.
+ **/
+gboolean
+gupnp_service_proxy_end_action_valist (GUPnPServiceProxy       *proxy,
+                                       GUPnPServiceProxyAction *action,
+                                       GError                 **error,
+                                       va_list                  var_args)
+{
+        g_return_val_if_fail (GUPNP_IS_SERVICE_PROXY (proxy), FALSE);
+        g_return_val_if_fail (action, FALSE);
+
+        return FALSE;
+}
+
+/**
+ * gupnp_service_proxy_cancel_action
+ * @proxy: A #GUPnPServiceProxy
+ * @action: A #GUPnPServiceProxyAction handle
+ *
+ * Cancels @action, freeing the @action handle.
+ **/
+void
+gupnp_service_proxy_cancel_action (GUPnPServiceProxy       *proxy,
+                                   GUPnPServiceProxyAction *action)
+{
+        g_return_if_fail (GUPNP_IS_SERVICE_PROXY (proxy));
+        g_return_if_fail (action);
+}
+
+/**
+ * gupnp_service_proxy_add_notify
+ * @proxy: A #GUPnPServiceProxy
+ * @variable: The variable to add notification for
+ * @callback: The callback to call when @variable changes
+ * @user_data: User data for @callback
+ *
+ * Sets up @callback to be called whenever a change notification for 
+ * @variable is recieved.
+ *
+ * Return value: A new notification ID.
+ **/
+guint
+gupnp_service_proxy_add_notify (GUPnPServiceProxy              *proxy,
+                                const char                     *variable,
+                                GUPnPServiceProxyNotifyCallback callback,
+                                gpointer                        user_data)
+{
+        g_return_val_if_fail (GUPNP_IS_SERVICE_PROXY (proxy), 0);
+        g_return_val_if_fail (variable, 0);
+        g_return_val_if_fail (callback, 0);
+
+        return 0;
+}
+
+/**
+ * gupnp_service_proxy_remove_notify
+ * @proxy: A #GUPnPServiceProxy
+ * @id: A notification ID
+ *
+ * Cancels the variable change notification with ID @id.
+ **/
+void
+gupnp_service_proxy_remove_notify (GUPnPServiceProxy *proxy,
+                                   guint              id)
+{
+        g_return_if_fail (GUPNP_IS_SERVICE_PROXY (proxy));
+        g_return_if_fail (id > 0);
+}
+
+/**
+ * gupnp_service_proxy_set_subscribed
+ * @proxy: A #GUPnPServiceProxy
+ * @subscribed: TRUE to subscribe to this service
+ *
+ * (Un)subscribes to this service.
+ **/
+void
+gupnp_service_proxy_set_subscribed (GUPnPServiceProxy *proxy,
+                                    gboolean           subscribed)
+{
+        g_return_if_fail (GUPNP_IS_SERVICE_PROXY (proxy));
+
+        proxy->priv->subscribed = subscribed;
+
+        g_object_notify (G_OBJECT (proxy), "subscribed");
+}
+
+/**
+ * gupnp_service_proxy_get_subscribed
+ * @proxy: A #GUPnPServiceProxy
+ *
+ * Return value: TRUE if we are subscribed to this service.
+ **/
+gboolean
+gupnp_service_proxy_get_subscribed (GUPnPServiceProxy *proxy)
+{
+        g_return_val_if_fail (GUPNP_IS_SERVICE_PROXY (proxy), FALSE);
+
+        return proxy->priv->subscribed;
 }
 
 /**
