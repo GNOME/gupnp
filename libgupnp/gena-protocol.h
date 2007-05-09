4b1bac6f843b29fd993a91e446d7193fb38bec62
Jorn Baayen <jorn@openedhand.com>
2007-05-09 18:19:48 +0000

    add missing file
    git-svn-id: https://svn.o-hand.com/repos/gupnp/gupnp@206 d8cb91d7-bff9-0310-92b9-80b65e4482b2

---
+++ libgupnp/gena-protocol.h
@@ -0,0 +1,35 @@
+/* 
+ * Copyright (C) 2007 OpenedHand Ltd.
+ *
+ * Author: Jorn Baayen <jorn@openedhand.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#ifndef __GENA_PROTOCOL_H__
+#define __GENA_PROTOCOL_H__
+
+G_BEGIN_DECLS
+
+#define GENA_METHOD_SUBSCRIBE   "SUBSCRIBE"
+#define GENA_METHOD_UNSUBSCRIBE "UNSUBSCRIBE"
+#define GENA_METHOD_NOTIFY      "NOTIFY"
+
+#define GENA_DEFAULT_TIMEOUT 1800
+
+G_END_DECLS
+
+#endif /* __GENA_PROTOCOL_H__ */
