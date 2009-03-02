3d3baad8e5413fa7d7bdd64967d420d1ed6efdcc
Zeeshan Ali (Khattak) <zeeshanak@gnome.org>
2009-03-02 17:18:47 +0200

    Separate out implementation from GUPnPContextManager.
    GUPnPContextManager now keeps a ref to another GUPnPContextManager (that
    has the actual network interface management implemenation) and proxies
    it's signals.

--- libgupnp/gupnp-context-manager.c
+++ libgupnp/gupnp-context-manager.c
@@ -34,22 +34,14 @@
 #include <config.h>
 #include <errno.h>
 #include <string.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <sys/utsname.h>
-#include <sys/ioctl.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <arpa/inet.h>
-#include <net/if.h>
-#include <ifaddrs.h>
 #include <libsoup/soup-address.h>
 #include <glib/gstdio.h>
 
 #include "gupnp-context-manager.h"
+#include "gupnp-context-manager-private.h"
+#include "gupnp-unix-context-manager.h"
 #include "gupnp-context.h"
 #include "gupnp-marshal.h"
 
@@ -61,12 +53,15 @@ struct _GUPnPContextManagerPrivate {
         GMainContext      *main_context;
 
         guint              port;
+
+        GUPnPContextManager *impl;
 };
 
 enum {
         PROP_0,
         PROP_MAIN_CONTEXT,
-        PROP_PORT
+        PROP_PORT,
+        PROP_CONTEXT_MANAGER
 };
 
 enum {
@@ -77,9 +72,9 @@ enum {
 
 static guint signals[SIGNAL_LAST];
 
-static void
-create_and_signal_context (GUPnPContextManager *manager,
-                           const char          *host_ip)
+void
+gupnp_context_manager_create_and_signal_context (GUPnPContextManager *manager,
+                                                 const char          *host_ip)
 {
         GUPnPContext *context;
         GError *error;
@@ -110,76 +105,32 @@ create_and_signal_context (GUPnPContextManager *manager,
         g_object_unref (context);
 }
 
-/* FIXME: We currently don't deal with IPv6 because libsoup doesn't seem to
- * like it and we get the screen full of warnings and errors. Please fix that
- * and uncomment the IPv6 handling here.
- */
-static char *
-get_host_ip_from_iface (struct ifaddrs *ifa)
+static void
+on_context_available (GUPnPContextManager *impl,
+                      GUPnPContext        *context,
+                      gpointer            *user_data)
 {
-        char ip[INET6_ADDRSTRLEN];
-        const char *host_ip = NULL;
-        struct sockaddr_in *s4;
-        /*struct sockaddr_in6 *s6;*/
+        GUPnPContextManager *manager = GUPNP_CONTEXT_MANAGER (user_data);
 
-        if (ifa->ifa_addr == NULL)
-                return NULL;
-
-        if (!(ifa->ifa_flags & IFF_UP))
-                return NULL;
-
-        switch (ifa->ifa_addr->sa_family) {
-                case AF_INET:
-                        s4 = (struct sockaddr_in *) ifa->ifa_addr;
-                        host_ip = inet_ntop (AF_INET,
-                                        &s4->sin_addr, ip, sizeof (ip));
-                        break;
-                /*case AF_INET6:
-                        s6 = (struct sockaddr_in6 *) ifa->ifa_addr;
-                        host_ip = inet_ntop (AF_INET6,
-                                        &s6->sin6_addr, ip, sizeof (ip));
-                        break;*/
-                default:
-                        break; /* Unknown: ignore */
-        }
-
-        return g_strdup (host_ip);
+        /* Just proxy the signal */
+        g_signal_emit (manager,
+                       signals[CONTEXT_AVAILABLE],
+                       0,
+                       context);
 }
 
-/*
- * Create a context for all network interfaces that are up.
- */
-static gboolean
-create_contexts (gpointer data)
+static void
+on_context_unavailable (GUPnPContextManager *impl,
+                        GUPnPContext        *context,
+                        gpointer            *user_data)
 {
-        GUPnPContextManager *manager = (GUPnPContextManager *) data;
-        struct ifaddrs *ifa_list, *ifa;
-        char *ret;
+        GUPnPContextManager *manager = GUPNP_CONTEXT_MANAGER (user_data);
 
-        ret = NULL;
-
-        if (getifaddrs (&ifa_list) != 0) {
-                g_warning ("Failed to retrieve list of network interfaces:%s\n",
-                           strerror (errno));
-
-                return FALSE;
-        }
-
-        for (ifa = ifa_list; ifa != NULL; ifa = ifa->ifa_next) {
-                char *host_ip;
-
-                host_ip = get_host_ip_from_iface (ifa);
-
-                if (host_ip != NULL) {
-                        create_and_signal_context (manager, host_ip);
-
-                        g_free (host_ip);
-                }
-        }
-
-        freeifaddrs (ifa_list);
-
-        return FALSE;
+        /* Just proxy the signal */
+        g_signal_emit (manager,
+                       signals[CONTEXT_UNAVAILABLE],
+                       0,
+                       context);
 }
 
 static void
@@ -199,16 +150,14 @@ gupnp_context_manager_constructor (GType                  type,
 	GObject *object;
 	GObjectClass *parent_class;
 	GUPnPContextManager *manager;
+	GUPnPContextManagerPrivate *priv;
 
 	parent_class = G_OBJECT_CLASS (gupnp_context_manager_parent_class);
 	object = parent_class->constructor (type, n_props, props);
 
         manager = GUPNP_CONTEXT_MANAGER (object);
 
-        /* Create contexts in mainloop so that is happens after user has hooked
-         * to the "context-available" signal.
-         */
-        g_idle_add (create_contexts, manager);
+        priv = manager->priv;
 
 	return object;
 }
@@ -220,15 +169,32 @@ gupnp_context_manager_set_property (GObject      *object,
                                     GParamSpec   *pspec)
 {
         GUPnPContextManager *manager;
+        GUPnPContextManagerPrivate *priv;
 
         manager = GUPNP_CONTEXT_MANAGER (object);
+        priv = manager->priv;
 
         switch (property_id) {
         case PROP_PORT:
-                manager->priv->port = g_value_get_uint (value);
+                priv->port = g_value_get_uint (value);
                 break;
         case PROP_MAIN_CONTEXT:
-                manager->priv->main_context = g_value_get_pointer (value);
+                priv->main_context = g_value_get_pointer (value);
+                break;
+        case PROP_CONTEXT_MANAGER:
+                priv->impl = g_value_get_object (value);
+                if (priv->impl != NULL) {
+                        priv->impl = g_object_ref (priv->impl);
+
+                        g_signal_connect (priv->impl,
+                                          "context-available",
+                                          G_CALLBACK (on_context_available),
+                                          manager);
+                        g_signal_connect (priv->impl,
+                                          "context-unavailable",
+                                          G_CALLBACK (on_context_unavailable),
+                                          manager);
+                }
                 break;
         default:
                 G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
@@ -253,6 +219,9 @@ gupnp_context_manager_get_property (GObject    *object,
         case PROP_MAIN_CONTEXT:
                 g_value_set_pointer (value, manager->priv->main_context);
                 break;
+        case PROP_CONTEXT_MANAGER:
+                g_value_set_object (value, manager->priv->impl);
+                break;
         default:
                 G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
                 break;
@@ -266,6 +235,11 @@ gupnp_context_manager_dispose (GObject *object)
         GObjectClass *object_class;
 
         manager = GUPNP_CONTEXT_MANAGER (object);
+
+        if (manager->priv->impl != NULL) {
+                g_object_unref (manager->priv->impl);
+                manager->priv->impl = NULL;
+        }
 
         /* Call super */
         object_class = G_OBJECT_CLASS (gupnp_context_manager_parent_class);
@@ -338,6 +312,25 @@ gupnp_context_manager_class_init (GUPnPContextManagerClass *klass)
                                     G_PARAM_STATIC_BLURB));
 
         /**
+         * GUPnPContextManager:context-manager
+         *
+         * The actual GUPnPContextManager implementation used.
+         **/
+        g_object_class_install_property
+                (object_class,
+                 PROP_CONTEXT_MANAGER,
+                 g_param_spec_object ("context-manager",
+                                      "ContextManager",
+                                      "ContextManager implemention",
+                                      GUPNP_TYPE_CONTEXT_MANAGER,
+                                      G_PARAM_WRITABLE |
+                                      G_PARAM_CONSTRUCT_ONLY |
+                                      G_PARAM_PRIVATE |
+                                      G_PARAM_STATIC_NAME |
+                                      G_PARAM_STATIC_NICK |
+                                      G_PARAM_STATIC_BLURB));
+
+        /**
          * GUPnPContextManager::context-available
          *
          * Signals the availability of new #GUPnPContext.
@@ -385,9 +378,21 @@ GUPnPContextManager *
 gupnp_context_manager_new (GMainContext *main_context,
                            guint         port)
 {
-        return g_object_new (GUPNP_TYPE_CONTEXT_MANAGER,
+        GUPnPContextManager *manager;
+        GUPnPContextManager *impl;
+
+        impl = g_object_new (GUPNP_TYPE_UNIX_CONTEXT_MANAGER,
                              "main-context", main_context,
                              "port", port,
                              NULL);
+
+        manager = g_object_new (GUPNP_TYPE_CONTEXT_MANAGER,
+                                "main-context", main_context,
+                                "port", port,
+                                "context-manager", impl,
+                                NULL);
+        g_object_unref (impl);
+
+        return manager;
 }
 
---
+++ libgupnp/gupnp-unix-context-manager.c
@@ -0,0 +1,179 @@
+/*
+ * Copyright (C) 2009 Nokia Corporation, all rights reserved.
+ * Copyright (C) 2006, 2007, 2008 OpenedHand Ltd.
+ *
+ * Author: Zeeshan Ali (Khattak) <zeeshanak@gnome.org>
+ *         Jorn Baayen <jorn@openedhand.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+/**
+ * SECTION:gupnp-unix-context-manager
+ * @short_description: Unix-specific implementation of #GUPnPContextManager.
+ *
+ */
+
+#include <config.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/utsname.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <arpa/inet.h>
+#include <net/if.h>
+#include <ifaddrs.h>
+#include <libsoup/soup-address.h>
+#include <glib/gstdio.h>
+
+#include "gupnp-context-manager-private.h"
+#include "gupnp-unix-context-manager.h"
+#include "gupnp-context.h"
+
+G_DEFINE_TYPE (GUPnPUnixContextManager,
+               gupnp_unix_context_manager,
+               GUPNP_TYPE_CONTEXT_MANAGER);
+
+/* FIXME: We currently don't deal with IPv6 because libsoup doesn't seem to
+ * like it and we get the screen full of warnings and errors. Please fix that
+ * and uncomment the IPv6 handling here.
+ */
+static char *
+get_host_ip_from_iface (struct ifaddrs *ifa)
+{
+        char ip[INET6_ADDRSTRLEN];
+        const char *host_ip = NULL;
+        struct sockaddr_in *s4;
+        /*struct sockaddr_in6 *s6;*/
+
+        if (ifa->ifa_addr == NULL)
+                return NULL;
+
+        if (!(ifa->ifa_flags & IFF_UP))
+                return NULL;
+
+        switch (ifa->ifa_addr->sa_family) {
+                case AF_INET:
+                        s4 = (struct sockaddr_in *) ifa->ifa_addr;
+                        host_ip = inet_ntop (AF_INET,
+                                        &s4->sin_addr, ip, sizeof (ip));
+                        break;
+                /*case AF_INET6:
+                        s6 = (struct sockaddr_in6 *) ifa->ifa_addr;
+                        host_ip = inet_ntop (AF_INET6,
+                                        &s6->sin6_addr, ip, sizeof (ip));
+                        break;*/
+                default:
+                        break; /* Unknown: ignore */
+        }
+
+        return g_strdup (host_ip);
+}
+
+/*
+ * Create a context for all network interfaces that are up.
+ */
+static gboolean
+create_contexts (gpointer data)
+{
+        GUPnPContextManager *manager = (GUPnPContextManager *) data;
+        struct ifaddrs *ifa_list, *ifa;
+        char *ret;
+
+        ret = NULL;
+
+        if (getifaddrs (&ifa_list) != 0) {
+                g_warning ("Failed to retrieve list of network interfaces:%s\n",
+                           strerror (errno));
+
+                return FALSE;
+        }
+
+        for (ifa = ifa_list; ifa != NULL; ifa = ifa->ifa_next) {
+                char *host_ip;
+
+                host_ip = get_host_ip_from_iface (ifa);
+
+                if (host_ip != NULL) {
+                        gupnp_context_manager_create_and_signal_context (
+                                                manager, host_ip);
+
+                        g_free (host_ip);
+                }
+        }
+
+        freeifaddrs (ifa_list);
+
+        return FALSE;
+}
+
+static void
+gupnp_unix_context_manager_init (GUPnPUnixContextManager *manager)
+{
+}
+
+static GObject *
+gupnp_unix_context_manager_constructor (GType                  type,
+                                        guint                  n_props,
+                                        GObjectConstructParam *props)
+{
+	GObject *object;
+	GObjectClass *parent_class;
+	GUPnPUnixContextManager *manager;
+
+	parent_class = G_OBJECT_CLASS (gupnp_unix_context_manager_parent_class);
+	object = parent_class->constructor (type, n_props, props);
+
+        manager = GUPNP_UNIX_CONTEXT_MANAGER (object);
+
+        /* Create contexts in mainloop so that is happens after user has hooked
+         * to the "context-available" signal.
+         */
+        g_idle_add (create_contexts, manager);
+
+	return object;
+}
+
+static void
+gupnp_unix_context_manager_dispose (GObject *object)
+{
+        GUPnPUnixContextManager *manager;
+        GObjectClass *object_class;
+
+        manager = GUPNP_UNIX_CONTEXT_MANAGER (object);
+
+        /* Call super */
+        object_class = G_OBJECT_CLASS (gupnp_unix_context_manager_parent_class);
+        object_class->dispose (object);
+}
+
+static void
+gupnp_unix_context_manager_class_init (GUPnPUnixContextManagerClass *klass)
+{
+        GObjectClass *object_class;
+
+        object_class = G_OBJECT_CLASS (klass);
+
+        object_class->constructor  = gupnp_unix_context_manager_constructor;
+        object_class->dispose      = gupnp_unix_context_manager_dispose;
+}
+
