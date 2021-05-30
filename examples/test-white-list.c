37b22d0a050842d91dc64678518c425b1e2a3451
Jens Georg <mail@jensge.org>
2021-05-30 12:39:04 +0200

    all: LGPL-2.0 -> LGPL-2.1
    Also, drop the wall of license text and use the SPDX identifier

--- examples/test-white-list.c
+++ examples/test-white-list.c
@@ -3,20 +3,7 @@
  *
  * Author: Ludovic Ferrandis <ludovic.ferrandis@intel.com>
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License for more details.
- *
- * You should have received a copy of the GNU Library General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- * Boston, MA 02110-1301, USA.
+ * SPDX-License-Identifier: LGPL-2.1-or-later
  */
 
 #include <libgupnp/gupnp-control-point.h>
@@ -182,6 +169,7 @@ print_white_list_entries(GUPnPWhiteList *wl)
         g_list_foreach (list, print_wl_entry, NULL);
         g_print ("\n");
 }
+
 
 static gboolean
 change_white_list(gpointer user_data)
--- libgupnp/gupnp-service-proxy-private.h
+++ libgupnp/gupnp-service-proxy-private.h
@@ -1,3 +1,6 @@
+/*
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
 #ifndef GUPNP_SERVICE_PROXY_PRIVATE_H
 #define GUPNP_SERVICE_PROXY_PRIVATE_H
 
